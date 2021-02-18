21e8d3e1ba7f8e6415440d5d097fa98a465fa11e
vagrant <gio_wan@live.ca>
2021-02-18 22:32:18 +0000

    add tree methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let awayFromOriginal = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      awayFromOriginal ++;
+    }
+    return awayFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal; 
   }
 
   /** Stretch **/
@@ -36,8 +44,40 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    // make an array of each vampire's ancestors
+    // go through the two arrays and return the first occurance of a common value
 
+    const thisAncestor = [];
+    let currentVampire = this;
+
+    thisAncestor.push(currentVampire);
+    while (currentVampire.creator) {
+      thisAncestor.push(currentVampire.creator);
+      currentVampire = currentVampire.creator;
+    }
+
+    const vampireAncestor = []
+    let currentVampire1 = vampire;
+    vampireAncestor.push(currentVampire1);
+    while (currentVampire1.creator) {
+      vampireAncestor.push(currentVampire1.creator);
+      currentVampire1 = currentVampire1.creator;
+    }
+
+    for (const ancestor of thisAncestor) {
+      for (const pointerAncestor of vampireAncestor) {
+        if (ancestor === pointerAncestor){
+          return ancestor;
+        }
+      }
+    }
   }
+
+
+
+
+
+
 }
 
 module.exports = Vampire;
